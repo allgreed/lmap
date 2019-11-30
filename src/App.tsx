293fce927a96e569b11f8e33197a596436e3421a
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-11-30 11:15:07 +0100

    PoC na kiju

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './core/main.ts';
+//import { Tree, Leaf } from './core/main.ts';
+
+// TODO: fix it
+interface URL {
+    address: string;
+    is_done: boolean;
+}
+
+interface Name {
+    content: string;
+    is_done: boolean;
+}
+
+export type Resource = URL | Name;
+
+export type Leaf = null;
+export type Tree = Leaf | [Resource, Tree[]];
+// endTODO
+
+const ble: Tree = [{content: "elon", is_done: false}, [
+    [{content: "mózg", is_done: true}, []],
+    [{content: "leci", is_done: false}, []],
+    [{content: "w marsa", is_done: true}, []],
+]];
+
+function fuj(t: Tree)
+{
+    // TODO: handle null
+    if (!t)
+        return;
+
+    // TODO: destruction syntax
+    const resource = t[0];
+    const subtrees = t[1];
+
+    return (
+        // TODO: wtf div
+        <div>
+            // TODO: handle unhandled properties
+            // TODO: typeguards is a protip
+            <li>[{resource.is_done ? "x" : " "}]</li>  // TODO: checkbox
+            <ul>
+                {subtrees.map(fuj)}
+            </ul>
+        </div>
+    )
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {fuj(ble)}
     </div>
   );
 }

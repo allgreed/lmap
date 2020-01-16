17f47c5c6e2a8884407cdd2e56c7700c03bef936
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2020-01-17 00:08:21 +0100

    Integrate visualization library with our datamodel

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,53 @@
+import { URL, Name, Resource } from './core/main';
+import { TreeNode } from './core/tree_methods';
 import React from 'react';
 import './App.css';
-import { URL, Name, Resource, Leaf, Tree } from './core/main';
+import 'react-tree-graph/dist/style.css';
+
+const Tree:any = require('react-tree-graph'); // missing external types, haxing it'!
 
 
-const ble: Tree = [{content: "elon", is_done: false}, [
-    [{content: "mózg", is_done: true}, [
-      [{content: "mozg1", is_done: false}, []],
-      [{content: "mozg2", is_done: false}, []],
-      [{content: "mozg3", is_done: false}, []]
-    ]],
-    [{content: "leci", is_done: false}, []],
-    [{content: "w marsa", is_done: true}, []],
-    [{content: "mózg", is_done: true}, []],
-    [{address: "https://", is_done: true}, []]
-]];
+let ourTree = new TreeNode("korzen")
+  .add(new TreeNode("notka1"))
+  .add(new TreeNode("notka2")
+    .add(new TreeNode("notka1od2"))
+    .add(new TreeNode("notka2od2")
+      .add(new TreeNode("notka1od22"))
+      .add(new TreeNode("notka2od22"))))
+  .add(new TreeNode("notka3")
+    .add(new TreeNode("notka1od3")));
 
-function fuj(t: Tree)
-{
-    // TODO: handle null
-    if (!t)
-        return;
 
-    // TODO: destruction syntax
-    const resource = t[0];
-    const subtrees = t[1];
-
-    return (
-        // TODO: wtf div
-        // TODO: handle unhandled properties
-        // TODO: typeguards is a protip
-        <div>
-
-            <li>
-              {resource && (resource as Name).content}
-              {(resource as URL).address}
-              {resource.is_done
-                ? <input type="checkbox" checked disabled />
-                : <input type="checkbox" disabled />}
-
-              </li>
-            <ul>
-                {subtrees.map(fuj)}
-            </ul>
-        </div>
-    )
+interface ReactTreeGraphNode {
+    name: String,
+    children: Array<ReactTreeGraphNode>,
 }
+
+
+function displayTree(t: TreeNode<String>): ReactTreeGraphNode
+{
+    const result: ReactTreeGraphNode = {
+        name: t.data,
+        children: [],
+    }
+
+    if (t.children.length !== 0)
+    {
+        result.children = t.children.map(displayTree)
+    }
+
+    return result;
+}
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      {fuj(ble)}
-    </div>
+        <Tree
+            data={displayTree(ourTree)}
+            height={400} // TODO: width and heigh as fullscreen - toolbar
+            width={400}/>
+            </div>
   );
 }
 

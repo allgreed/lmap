28f897732588c9413d7966ef6e8345576c7d9b2e
rufi156 <33355829+rufi156@users.noreply.github.com>
2020-01-16 19:42:38 +0100

    Tree methods (#8)
    * add methods tests
    
    * correct names
    
    * improve tests
    
    * var to let
    
    * change test implementation
    
    * add lodash
    
    * remove .length dependency
    
    * add is_done test; move lodash to TreeNode class
    
    * add fluent builder
    
    * start working on generic class
    
    * changing class into generic
    
    * add @types/lodash
    
    * add compilable class constructor
    
    * add generic class/add/remove; not compiling
    
    * change for loops to array methods
    
    * attempt pass fn as argument
    
    * Fix function passing
    
    * BOOM! generic tree_methods
    
    * content to data
    
    * add filter method; no compile
    
    * remove generic type from class methods
    
    Co-authored-by: Olgierd Kasprowicz <olixem@gmail.com>

---
+++ src/core/tree_methods.test.ts
@@ -0,0 +1,51 @@
+import * as tree from './tree_methods'
+
+
+let treeRoot = new tree.TreeNode("korzen")
+  .add(new tree.TreeNode("notka1"))
+  .add(new tree.TreeNode("notka2")
+    .add(new tree.TreeNode("notka1od2"))
+    .add(new tree.TreeNode("notka2od2")
+      .add(new tree.TreeNode("notka1od22"))
+      .add(new tree.TreeNode("notka2od22"))))
+  .add(new tree.TreeNode("notka3")
+    .add(new tree.TreeNode("notka1od3")));
+
+
+test("count tree nodes", () => {
+  let root = treeRoot.clone();
+  expect(tree.howManyTreeNodes(root)).toEqual(8);
+});
+
+test("add node", () => {
+  let root = treeRoot.clone();
+  let node = root.children.filter(child => (child.data === "notka3"))[0]
+  node.add(new tree.TreeNode("notatka2od3"));
+  expect(node.children.some(child => (child.data === "notatka2od3"))).toEqual(true);
+  expect(tree.howManyTreeNodes(root)).toEqual(9);
+});
+
+test("find node with string", () => {
+  let root = treeRoot.clone();
+  let nodesWithString: TreeNode[] = root.filter((child) => child.data.includes("1od"));
+  expect(nodesWithString.every(obj => obj.data.includes("1od"))).toEqual(true);
+});
+
+test("find parent of a node", () => {
+  let root = treeRoot.clone();
+  let exampleNode = root.filter((child) => child.data.includes("1od"))[0];
+  let exampleParent = tree.getParent(exampleNode, root);
+  expect(exampleParent.data).toEqual("notka2");
+});
+
+test("remove node", () => {
+  let root = treeRoot.clone();
+  let exampleNode = root.filter((child) => child.data.includes("1od"))[0];
+  let x: TreeNode[] = root.filter((child) => child.data.includes("2od2"))[0];
+  let xChildren = x.children;
+  let parent: TreeNode = tree.getParent(x, root)
+  tree.getParent(x, root).removeNode(x);
+  expect(tree.howManyTreeNodes(root)).toEqual(7);
+  expect(parent.children.some(child => child.data == "notka2od2")).toEqual(false);
+  expect(parent.children).toEqual(expect.arrayContaining(xChildren));
+});
---
+++ src/core/tree_methods.ts
@@ -0,0 +1,75 @@
+import * as _ from 'lodash'
+
+export class TreeNode<T> {
+  data: T;
+  children: TreeNode<T>[];
+
+  constructor(data: T, children: TreeNode<T>[] = []) {
+    this.data = data;
+    this.children = children;
+  }
+  add(node: TreeNode<T>): TreeNode<T> {
+    this.children.push(node);
+    return this;
+  }
+  removeTree(node: TreeNode<T>) {
+  //remove node with children
+    let  chosenChildIndex = node.children.findIndex(child => child === node);
+    this.children.splice(chosenChildIndex, 1);
+  }
+  removeNode(node: TreeNode<T>): number {
+  //remove node and attach its any children to this.
+  //return -1 if node doesnt exist
+    let  chosenChild = this.children.find(child => child == node);
+    if (chosenChild != undefined) {
+      let  chosenChildIndex = this.children.indexOf(chosenChild);
+      chosenChild.children.forEach(grandChild => this.add(grandChild));
+      this.children.splice(chosenChildIndex, 1);
+      return 0;
+    }else {
+      return -1;
+    }
+
+  }
+  clone() {
+    return _.cloneDeep(this);
+  }
+  filterREC(f: (arg: TreeNode<T>) => boolean, results: TreeNode<T>[]){
+  //for filter()
+    this.children.forEach(function(child){
+      if (f(child)) {
+        results.push(child);
+      }
+      child.filterREC(f, results);
+    });
+  }
+  filter(f: (arg: TreeNode<T>) => boolean): TreeNode<T>[] {
+  //for root obj
+    let searchResults: TreeNode<T>[] = [];
+    this.filterREC(f, searchResults);
+    return searchResults;
+  }
+}
+
+export function getParentREC<T>(node: TreeNode<T>, root: TreeNode<T>, parentList: TreeNode<T>[]) {
+  root.children.forEach((child) => {
+    if (child == node) {
+      parentList.push(root);
+    }
+    getParentREC(node, child, parentList);
+  });
+}
+
+export function getParent<T>(node: TreeNode<T>, root: TreeNode<T>): TreeNode<T> {
+  let parent: TreeNode<T>[] = [];
+  getParentREC(node, root, parent);
+  return parent[0];
+}
+
+export function howManyTreeNodes<T>(root: TreeNode<T>): number {
+  let no: number = root.children.length;
+  root.children.forEach(function(child){
+    no += howManyTreeNodes(child);
+  });
+  return no;
+}

de4acf1cdd728f053c5793c2d7e0020f475df484
rufi156 <33355829+rufi156@users.noreply.github.com>
2020-01-25 11:23:29 +0100

    Lechia (#13)
    * Add some pocy poc!
    
    * Something is wrong with removal xD
    
    * display clicked node
    
    * adding nodes
    
    * Try to add onChange
    
    * Add upload
    
    Co-authored-by: Olgierd Kasprowicz <olixem@gmail.com>
    Co-authored-by: AdrianRybandt <adrian.rybandt@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import { URL, Name, Resource } from './core/main';
 import { TreeNode } from './core/tree_methods';
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'react-tree-graph/dist/style.css';
 
 const Tree:any = require('react-tree-graph'); // missing external types, haxing it'!
-
-
-let ourTree = new TreeNode("korzen")
-  .add(new TreeNode("notka1"))
-  .add(new TreeNode("notka2")
-    .add(new TreeNode("notka1od2"))
-    .add(new TreeNode("notka2od2")
-      .add(new TreeNode("notka1od22"))
-      .add(new TreeNode("notka2od22"))))
-  .add(new TreeNode("notka3")
-    .add(new TreeNode("notka1od3")));
 
 
 interface ReactTreeGraphNode {
@@ -39,16 +28,68 @@ function displayTree(t: TreeNode<String>): ReactTreeGraphNode
     return result;
 }
 
+export default class App extends Component<{}, { chosenNode: TreeNode<string>, value: string, count: number, ourTree: TreeNode<string>}>
+{
+  constructor(props: any){
+    super(props);
+    this.state= {
+      chosenNode: new TreeNode(""),
+      value: 'Name',
+      count:0,
+      ourTree: new TreeNode("korzen")
+  .add(new TreeNode("notka1"))
+  .add(new TreeNode("notka2")
+    .add(new TreeNode("notka1od2"))
+    .add(new TreeNode("notka2od2")
+      .add(new TreeNode("notka1od22"))
+      .add(new TreeNode("notka2od22"))))
+  .add(new TreeNode("notka3")
+    .add(new TreeNode("notka1od3"))),
+	};
+	this.handleChange = this.handleChange.bind(this);
+  }
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-        <Tree
-            data={displayTree(ourTree)}
-            height={400} // TODO: width and heigh as fullscreen - toolbar
-            width={400}/>
-            </div>
-  );
+    handleChange(event: any) {
+    this.setState({value: event.target.value});
+    event.preventDefault();
+    }
+
+
+
+
+  ubij_noda(event: any, node_key: string, value: string)
+  {
+    this.state.ourTree.filter(n => n.data === node_key)[0].add(new TreeNode(value))
+
+    this.setState({
+        ourTree: this.state.ourTree
+    })
+
+  }
+
+  przepiszNoda(event: any, node_key: string) {
+    this.setState({
+        chosenNode: this.state.ourTree.filter(n => n.data === node_key)[0]
+    })
+  }
+
+  render()
+  {
+      return (
+        <div className="App">
+            <Tree
+                data={displayTree(this.state.ourTree)}
+                height={400} // TODO: width and heigh as fullscreen - toolbar
+                gProps={{
+                    onClick: this.przepiszNoda.bind(this)
+                }}
+                width={400}/>
+		
+            <label>Node:</label>
+            <input type="text" name="node" value={this.state.value} onChange={this.handleChange}/>
+            <button onClick = { e => this.ubij_noda(e, this.state.chosenNode.data, this.state.value) }>Dodaj</button>
+
+        </div>
+      );
+  }
 }
-
-export default App;

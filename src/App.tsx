4d1f76b618ab3c32ec88256b3df5c9056c6c3786
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2020-04-20 20:05:22 +0200

    Implement the proxy in terms of raw TreeNodes
    There where some sloppy hacks involved, we will iron them out further
    down the road. This allows me to add a proper deterministic id provider
    by centralising id service for a Tree - an aggregate over TreeNodes.
    This also serves as a facade which should make further implementation
    changes independent of the UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { URL, Name, Resource } from "./core/main";
-import { Tree, makeTree} from "./core/tree_methods";
+import { Tree, makeTree, TreeNode } from "./core/tree_methods";
 import React, { Component } from "react";
 import "./App.css";
 import "react-tree-graph/dist/style.css";
@@ -19,84 +19,93 @@ interface bleble {
 
 function displayTree(t: Tree<bleble>): ReactTreeGraphNode
 {
-    const result: ReactTreeGraphNode = {
-        name: t.data.name,
-        id: t.id,
-        children: [],
-    }
-
-    if (t.children.length !== 0)
+    function _displayTree(t: TreeNode<bleble>): ReactTreeGraphNode
     {
-        result.children = t.children.map(displayTree)
+        const result: ReactTreeGraphNode = {
+            name: t.data.name,
+            id: t.id,
+            children: [],
+        }
+
+        if (t.children.length !== 0)
+        {
+            result.children = t.children.map(_displayTree)
+        }
+
+        return result;
     }
 
-    return result;
+    return _displayTree(t.root);
 }
 
-export default class App extends Component<{}, { chosenNode: Tree<bleble>, value: string, count: number, ourTree: Tree<bleble>}>
+// TODO: use NodeID type instead of bare number
+export default class App extends Component<{}, { chosenNode: number, value: string, count: number, ourTree: Tree<bleble>}>
 {
     constructor(props: any)
     {
         super(props);
         this.state= {
-            chosenNode: makeTree({name: ""}),
+            // TODO: replace with a proper[ty] xD
+            chosenNode: Number.MIN_SAFE_INTEGER,
             value: "Name",
             count:0,
             ourTree: makeTree({name: "korzen"})
-                .add({name: "notka1"})
-                .add({name: "notka2"})
-                .addTree(makeTree({name: "notka3"})
-                    .add({name: "notka1od3"})
-                    .add({name: "notka2od3"}))
-                .addTree(makeTree({name: "notka4"})
-                    .add({name: "notka1od4"})
-                    .add({name: "notka2od4"})
-                    .addTree(makeTree({name: "notka3od4"})
-                        .add({name: "notka1od34"})
-                        .add({name: "notka2od34"}))
-                    .add({name: "notka5"})),
+                .addToRoot({name: "notka1"})
+                .addToRoot({name: "notka2"})
+                .addTreeToRoot(makeTree({name: "notka3"})
+                    .addToRoot({name: "notka1od3"})
+                    .addToRoot({name: "notka2od3"}))
+                .addTreeToRoot(makeTree({name: "notka4"})
+                    .addToRoot({name: "notka1od4"})
+                    .addToRoot({name: "notka2od4"})
+                    .addTreeToRoot(makeTree({name: "notka3od4"})
+                        .addToRoot({name: "notka1od34"})
+                        .addToRoot({name: "notka2od34"}))
+                    .addToRoot({name: "notka5"})),
         
         };
         this.handleChange = this.handleChange.bind(this);
     }
 
+    // TODO: ffs
     handleChange(event: any) 
     {
         this.setState({value: event.target.value});
         event.preventDefault();
     }
 
-
     addCustom(event: any, node_id: number, value: string)
     {
-        this.state.ourTree.flatten().filter(n => n.id === node_id)[0].add({name: value})
+        this.state.ourTree.add(node_id, {name: value});
 
     }
 
     remove(event: any)
     {
-        this.state.ourTree.getParent(this.state.chosenNode, this.state.ourTree).removeNode(this.state.chosenNode)
+        this.state.ourTree.removeNode(this.state.chosenNode);
 
+        // TODO: select parent of the deleted node
         this.setState({
             ourTree: this.state.ourTree,
-            chosenNode: this.state.ourTree
-        })
+            chosenNode: Number.MIN_SAFE_INTEGER,
+        });
     }
 
     selectNode(event: any, node_id: number)
     {
         this.setState({
-            chosenNode: node_id === this.state.ourTree.id ? this.state.ourTree : this.state.ourTree.flatten().filter(n => n.id === node_id)[0]
-        })
+            chosenNode: node_id,
+        });
     }
 
     editNode(event: any, value: string)
     {
-        this.state.chosenNode.data.name = value
+        // TODO: get rid of private access
+        this.state.ourTree._selectNodeById(this.state.chosenNode).data.name = value;
 
         this.setState({
-            ourTree: this.state.ourTree
-        })
+            ourTree: this.state.ourTree,
+        });
 
     }
 
@@ -114,10 +123,12 @@ export default class App extends Component<{}, { chosenNode: Tree<bleble>, value
                     keyProp="id"
                 />
 
-                <label>Selected node: {this.state.chosenNode.data.name} : {this.state.chosenNode.id}</label>
+                {// TODO: get rid of private access
+                }
+                <label>Selected node: {this.state.ourTree._selectNodeById(this.state.chosenNode).data.name} : {this.state.chosenNode}</label>
                 <button onClick = { e => this.remove(e) }>Usuń</button>
                 <input type="text" name="node" value={this.state.value} onChange={this.handleChange}/>
-                <button onClick = { e => this.addCustom(e, this.state.chosenNode.id, this.state.value) }>Dodaj</button>
+                <button onClick = { e => this.addCustom(e, this.state.chosenNode, this.state.value) }>Dodaj</button>
                 <button onClick = { e => this.editNode(e, this.state.value) }>Edytuj</button>
 
             </div>
--- src/core/tree_methods.test.ts
+++ src/core/tree_methods.test.ts
@@ -1,92 +1,100 @@
-import { makeTree } from "./tree_methods"
+import { Tree, makeTree } from "./tree_methods"
 
 interface testData{
     name: string,
 };
 
 let treeRoot = makeTree({name: "korzen"})
-    .add({name: "notka1"})
-    .add({name: "notka2"})
-    .addTree(makeTree({name: "notka3"})
-        .add({name: "notka1od3"})
-        .add({name: "notka2od3"}))
-    .addTree(makeTree({name: "notka4"})
-        .add({name: "notka1od4"})
-        .add({name: "notka2od4"})
-        .addTree(makeTree({name: "notka3od4"})
-            .add({name: "notka1od34"})
-            .add({name: "notka2od34"}))
-        .add({name: "notka5"}));
+    .addToRoot({name: "notka1"})
+    .addToRoot({name: "notka2"})
+    .addTreeToRoot(makeTree({name: "notka3"})
+        .addToRoot({name: "notka1od3"})
+        .addToRoot({name: "notka2od3"}))
+    .addTreeToRoot(makeTree({name: "notka4"})
+        .addToRoot({name: "notka1od4"})
+        .addToRoot({name: "notka2od4"})
+        .addTreeToRoot(makeTree({name: "notka3od4"})
+            .addToRoot({name: "notka1od34"})
+            .addToRoot({name: "notka2od34"}))
+        .addToRoot({name: "notka5"}));
 
 
 test("count tree nodes", () => 
 {
-    let root = treeRoot.clone();
+    const root = treeRoot.clone();
     expect(root.length).toEqual(12);
 });
 
 test("add node", () => 
 {
-    let root = treeRoot.clone();
-    let node = root.children.filter(child => (child.data.name === "notka3"))[0]
-    node.add({name: "notatka2od3"});
+    const tree = treeRoot.clone();
+    // TODO: less fucked up way of selecting a specific node inside a tree?
+    const node = tree.root.children.filter(child => (child.data.name === "notka3"))[0]
+
+    tree.add(node.id, {name: "notatka2od3"});
+
+    // TODO: make literal comparison
     expect(node.children.some(child => (child.data.name === "notatka2od3"))).toEqual(true);
 });
 
 test("add tree", () =>
 {
-    // TODO: put the id provider on a class and inject it in one place, not 4
-    const sequentialIdProvider = [1, 2, 3, 4, 22, 33, 44]
+    const sequentialIdProvider = [3, 4, 22, 33, 44]
         .reduce((acc, cur) => acc.mockReturnValueOnce(cur), jest.fn())
+    const _makeTree = _ => makeTree(_, { idProvider: sequentialIdProvider });
 
-    const root = makeTree("", sequentialIdProvider); 
-    const initialRootId = root.id;
-    const otherTree = makeTree("other", sequentialIdProvider)
-        .add("adjin", sequentialIdProvider)
-        .add("dwa", sequentialIdProvider);
-    expect(sequentialIdProvider.mock.calls.length).toBe(4);
+    const otherTree = _makeTree("other")
+        .addToRoot("adjin")
+        .addToRoot("dwa");
+    expect(sequentialIdProvider.mock.calls.length).toBe(2);
 
-    root.addTree(otherTree, sequentialIdProvider); 
+    const tree = _makeTree("").addTreeToRoot(otherTree, sequentialIdProvider);
 
-    expect(sequentialIdProvider.mock.calls.length).toBe(7);
-    expect(root.id).toBe(initialRootId);
-    expect(root.children[0].id).toBe(22);
-    expect(root.children[0].children.map(node => node.id)).toStrictEqual([33, 44]);
+    expect(sequentialIdProvider.mock.calls.length).toBe(5);
+    expect(tree.flatten().sort().map(node => node.id)).toStrictEqual([-9007199254740991, 22, 33, 44]);
 });
 
 test("flatten a tree", () =>
 {
-    let root = treeRoot.clone();
-    let flattenedTree = root.flatten();
+    const tree = treeRoot.clone();
+
+    const flattenedTree = tree.flatten();
+
+    // TODO: make literal comparison - use Set
     expect(flattenedTree.length).toEqual(13)
 })
 
 test("find node with string", () => 
 {
-    let root = treeRoot.clone();
-    let nodesWithString = root.flatten().filter((child) => child.data.name.includes("1od"));
-    expect(nodesWithString.every(obj => obj.data.name.includes("1od"))).toEqual(true);
+    const tree = treeRoot.clone();
+
+    const nodesWithString = tree.flatten().filter(node => node.data.name.includes("1od"));
+
+    // TODO: make literal comparison
+    expect(nodesWithString.every(node => node.data.name.includes("1od"))).toEqual(true);
 });
 
+// TODO: should this be tested or is implementation detail?
 test("find parent of a node", () => 
 {
-    let root = treeRoot.clone();
-    let exampleNode = root.flatten().filter((child) => child.data.name.includes("notka1od3"))[0];
-    let exampleParent = exampleNode.getParent(exampleNode, root);
-    expect(exampleParent.data.name).toEqual("notka3");
+    const tree = treeRoot.clone();
+    const node = tree.flatten().filter(node => node.data.name.includes("notka1od3"))[0];
+
+    // TODO: uncrap the getParent API
+    expect(node.getParent(node, tree.root).data.name).toEqual("notka3");
 });
 
 test("remove node", () => 
 {
-    let root = treeRoot.clone();
-    let x = root.flatten().filter((child) => child.data.name.includes("notka3od4"))[0];
-    let xChildren = x.children;
-    let parent = root.getParent(x, root) // notka4
-    let lenBeforeRemoval = root.length
+    const tree = treeRoot.clone();
+    const x = tree.flatten().filter((child) => child.data.name.includes("notka3od4"))[0];
+    const xChildren = x.children;
+    const parent = x.getParent(x, tree.root) // notka4
+    const lenBeforeRemoval = tree.length
 
-    root.getParent(x, root).removeNode(x);
+    const treeWithoutANode = tree.removeNode(x.id);
 
-    expect(root.length).toEqual(lenBeforeRemoval - 1);
+    expect(treeWithoutANode.length).toEqual(lenBeforeRemoval - 1);
     expect(parent.children.some(child => child.data.name === "notka3od4")).toEqual(false);
     // TODO: check rest of the tree, check order of items in tree
     //expect(parent.children).toEqual(expect.arrayContaining(xChildren));

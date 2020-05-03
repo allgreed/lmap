927de153b3eb932d655999760e6867f058189bc0
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2020-05-03 02:58:00 +0200

    Extract ResourceEditor component
    This commit doesn't change anything functionally
    
    The main component was getting slightly out of hand. This also allowed
    me to establish cleaner boundaries between domain objects - the
    ResourceEditor is unaware of any Tree (and so would be TreeDisplay after
    extraction) and the main component couldn't care less about what exactly
    resources are [aside from creating the initial tree]
    
    Had some issues with synchronizing changes on the address filed of the
    form, yet the key approach (https://stackoverflow.com/a/53313430/9134286)
    yielded good results. Conceptually you can think of it as both:
    - an array of ResourceEditors that happens to haven only one element
    - the ResourceEditor being spun up for every node that has a potential
    to be edited by the user (which right now is the selected node) and
    discarded when not needed
    
    I'd like to than my former employer for making processors that allow
    this a bit wasteful, yet marvelously elegant design

---
+++ src/ResourceEditor.tsx
@@ -0,0 +1,69 @@
+import { Link, Resource } from "./core/resources";
+import React, { Component } from "react";
+
+import "./ResourceEditor.css";
+
+
+export default class ResourceEditor extends Component<{
+    resource: Resource,
+    isDeletable: boolean,
+    onDelete: () => void,
+    onAdd: (resource: Resource) => void,
+    onEditonCommit: (resource: Resource) => void
+}, {
+    resource: Resource,
+}>
+{
+    // TODO: what is the correct props type? o.0
+    // TODO: can I type the props once?
+    constructor(props: any)
+    {
+        super(props);
+
+        this.state = {
+            resource: this.props.resource,
+        };
+    }
+
+    handleChange = (event: React.FormEvent<HTMLInputElement>) =>
+    {
+        this.setState({
+            resource: {
+                ...this.state.resource,
+                address: event.currentTarget.value,
+            }
+        })
+    }    
+
+    render()
+    {
+        return(
+            <div>
+                {
+                    // TODO: remove the hax and organize css classes
+                    // TODO: clean App.css from NodeEditor-specific stuff
+                }
+                {
+                    // TODO: editor should reflect on resource type
+                    // TODO: add a possiblity to select type to add (then it's just editor)
+                    // right now it's fixed to link
+                }
+                <label className="file-uploader-label">address</label>
+                <input 
+                    type="text"
+                    defaultValue={(this.props.resource as Link).address}
+                    onChange={ this.handleChange }
+                />
+                <div className="block-hax"></div>
+                <button 
+                    disabled={this.props.isDeletable}
+                    onClick={ _ => this.props.onDelete()}
+                >
+                    Usuń
+                </button>
+                <button onClick={ _ => this.props.onAdd(this.state.resource) }> Dodaj</button>
+                <button onClick={ _ => this.props.onEditonCommit(this.state.resource) }>Edytuj</button>
+            </div>
+        );
+    }
+}

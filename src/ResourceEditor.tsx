ee415b3d53b296448c1d449ed943a90303b8ea3e
Olgierd Kasprowicz <olixem@gmail.com>
2020-05-08 00:13:48 +0200

    Add safeguards for future Resource addition (#55)
    When adding a new Resource correctly (see comments in the resources.ts file)
    the type system demands an entry for every components that does
    type-based pattern matching on the Resource discriminated union members
    
    Inspiration:
    https://medium.com/@fillopeter/pattern-matching-with-typescript-done-right-94049ddd671c#809c
    
    Limitations:
    - the solution  doesn't check for excessive declarations / code handling
     a non-existing resource
    - casting, casting everywhere, but we'll deal with this later
    - two additional sets with Resource addition (aside from actually adding
    the Resource)
    
    I'm accepting the tradeof as adding Resources shouldn't be that common
    and there is a comment in place to facilitate doing that correctly.
    To the best of my knowledge this is the best that could be done
    
    It actually allowed to simplify many mechanisms either directly (moving
    the statically reflected type properties to the core domain) or
    indirectly (I've realised that initial Formik fields can be simply the
    resource object itself).
    
    As a side note: I'm having second thoughts about choosing TypeScript for
    this, as it clearly lacks the expressive power to implement such
    features properly

--- src/ResourceEditor.tsx
+++ src/ResourceEditor.tsx
@@ -16,13 +16,15 @@ const ResourceEditor: React.FunctionComponent<{
 }> = (props) =>
     <Formik
         initialValues={
-            initial_fields_for_resource(props.resource)
+            props.resource
         }
         onSubmit={(values, actions) =>
         {
             const resource = {
                 ...values,
-                is_done: false,
+                // TODO: remove this after #35
+                is_done: props.resource.is_done,
+                __typename: props.resource.__typename,
             };
 
             props.onEditonCommit(resource as any);
@@ -43,45 +45,18 @@ const ResourceEditor: React.FunctionComponent<{
 export default ResourceEditor;
 
 
-// TODO: use reflection instead of hardcode
 // TODO: is the filedset a good idea here?
+// TODO: dry-ify this a bit
 function resource_to_fields(r: Resource): Array<JSX.Element>
 {
-    return [
-        {
-            property: "address",
-            jsx: <fieldset>
-                <label className="file-uploader-label">address</label>
-                <Field name="address" />
-            </fieldset>
-        },
-        {
-            property: "content",
-            jsx: <fieldset>
-                <label className="file-uploader-label">content</label>
-                <Field name="content" />
-            </fieldset>
-        },
-    ]
-        .filter(x => x.property in r)
-        .map(x => x.jsx)
-    ;
-}
-
-// TODO: use reflection instead of hardcode
-// TODO: return type will actually be a Resource 0.o
-function initial_fields_for_resource(r: Resource): object
-{
-    return [
-        "address",
-        "content",
-    ]
-        .filter(x => x in r)
-        .reduce((obj, prop) =>
-        {
-            // TODO: do something about the types
-            (obj as any)[prop] = (r as any)[prop];
-            return obj;
-        }, {})
-    ;
+    return {
+        "Link": [<fieldset>
+            <label className="file-uploader-label">address</label>
+            <Field name="address" />
+        </fieldset>],
+        "Text": [<fieldset>
+            <label className="file-uploader-label">content</label>
+            <Field name="content" />
+        </fieldset>],
+    }[r.__typename];
 }
--- src/core/resources.ts
+++ src/core/resources.ts
@@ -1,16 +1,28 @@
 export interface Link {
+    __typename: "Link";
     address: string;
     is_done: boolean;
 }
 
 export interface Text {
+    __typename: "Text";
     content: string;
     is_done: boolean;
-    // TODO: this is an enum, not a boolean
+    // TODO: is_done => status ; boolean => enum
 }
+// when adding a new Resource please remember to add it to Resource union
+// and ResourceTypeStringValues array
 
 // TODO: add a null resource - for the root
 
+
 export type Resource = Link | Text;
+export type ResourceTypeString = Resource["__typename"];
+
+export const ResourceTypeStringValues: Array<ResourceTypeString> = [
+    "Link",
+    "Text",
+];
+
 
 export default {};
